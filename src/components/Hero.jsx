85cd03762254a1100d0f9612d9f4686117aff980
JawadHaider019 <JawadHaider019>
2026-03-11 17:22:17 +0500

    add about services and contact page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -196,7 +196,7 @@ const Hero = () => {
                   <div 
                     className="w-full h-full bg-cover bg-center"
                     style={{
-                      backgroundImage: "url('https://images.unsplash.com/photo-1581092918056-0c4c3acd3789?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80')"
+                      backgroundImage: "url('https://images.unsplash.com/photo-1553877522-43269d4ea984?q=80&w=2070&auto=format&fit=crop')"
                     }}
                   ></div>
                 </div>
